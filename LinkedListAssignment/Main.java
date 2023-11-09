a8082fe6bc4b35f2e67eb6fdc0fc929ea21e7558
胡亚崑 <116873082+yakun-hu@users.noreply.github.com>
2023-11-09 15:38:16 -0500

    Add files via upload
    Function calls

---
+++ LinkedListAssignment/Main.java
@@ -0,0 +1,16 @@
+package LinkedList2;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		LL list = new LL(); 
+		list.addend(30);		
+		list.addend(20);		
+		list.addend(10); 
+		list.addend(0); 
+		list.delend();
+		list.display();
+	}
+
+}
